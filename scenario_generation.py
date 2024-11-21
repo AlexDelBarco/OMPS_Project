eef72ff8d380a763e69601f6f186d0a1af36ac74
matthiasgroeller <s242880@dtu.dk>
2024-11-21 14:48:54 +0100

    First implementation of the two stage stochastic programm and altering the code towards the scenario_generation.py data structure

--- scenario_generation.py
+++ scenario_generation.py
@@ -6,145 +6,185 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
-np.random.seed(1)
+def createScenarios():
+    np.random.seed(1)
+
+    prices_data = [
+        51.49, 48.39, 48.92, 49.45, 42.72, 50.84, 82.15, 100.96, 116.60,
+        112.20, 108.54, 111.61, 114.02, 127.40, 134.02, 142.18, 147.42,
+        155.91, 154.10, 148.30, 138.59, 129.44, 122.89, 112.47
 
 
-prices_data_DA = [
-    51.49, 48.39, 48.92, 49.45, 42.72, 50.84, 82.15, 100.96, 116.60,
-    112.20, 108.54, 111.61, 114.02, 127.40, 134.02, 142.18, 147.42,
-    155.91, 154.10, 148.30, 138.59, 129.44, 122.89, 112.47
-]
-#TODO
-prices_data_B = [
-    51.49, 108.89, 90.76, 90.76, 42.72, -2.00, 11.89, 40.50, 67.98, 42.00, 42.00,
-    46.92, 114.02, 127.40, 134.02, 142.18, 198.65, 402.30, 154.10, 148.30,
-    138.59, 129.44, 122.89, 112.47
+    ]
 
-]
+    #TODO increase in necessary
+    num_price_scenarios = 10
+    num_scenarios_wind = 10
+
+    SCENARIOS_PRICE = [f'"P{k}"' for k in range(1, num_price_scenarios + 1)]
+    SCENARIOS_WIND = [f'"V{k}"' for k in range(1, num_scenarios_wind + 1)]
+
+    num_scenarios_total = num_price_scenarios * num_scenarios_wind
+
+    SCENARIOS_TOT = [k for k in range(1, num_scenarios_total + 1)]
+
+    SCENARIOS = np.sort(np.random.choice(np.arange(1, num_scenarios_total + 1), size=num_scenarios_total, replace=False))
+
+    ## Generate price scenarios using normal distribution
+    price_scenarios = {}
+    k_x = 0
+    for k in SCENARIOS_PRICE:
+        k_x = k_x + 1
+        plot_values_k = []
+        for t in range(1, 25):
+            coeff_prices = abs(np.random.normal(1, 0.1))
+            price_scenarios[t, k] = coeff_prices * prices_data[t - 1]
+
+    ### WT Power definition
+    WIND_FARMS = ['WT1', 'WT2', 'WT3', 'WT4', 'WT5', 'WT6']
+
+    dfs = {}
+    wind_power_scenarios = {}
+    p_max_wind = 48/6
+
+    for k in range(1, 7):
+        file_path = f'data/wind {k}.out'
+
+        with open(file_path, 'r') as file:
+            file_contents = file.readlines()
+            data = [line.strip().split(",") for line in file_contents]
+            df = pd.DataFrame(data)
+            df = df.iloc[:, 1:]
+            df.columns = df.iloc[0]
+            df = df.iloc[1:25, :num_scenarios_wind]
+            df = df.reset_index(drop=True)
+            df = df.apply(pd.to_numeric, errors='coerce')
+            dfs[f'WT{k}'] = df * p_max_wind
+
+    # plt.figure(figsize=(12, 12))
+    k_x = 0
+    for k in SCENARIOS_WIND:
+        k_x = k_x + 1
+        plot_values_k = []
+        for t in range(1, 25):
+            wind_power_scenarios[t, k] = sum(dfs[wt][k][t - 1] for wt in WIND_FARMS)
 
 
-#TODO increase in necessary
-num_price_scenarios = 10
-num_scenarios_wind = 10
 
-SCENARIOS_PRICE = [f'"P{k}"' for k in range(1, num_price_scenarios + 1)]
-SCENARIOS_WIND = [f'"V{k}"' for k in range(1, num_scenarios_wind + 1)]
 
-num_scenarios_total = num_price_scenarios * num_scenarios_wind
 
-SCENARIOS_TOT = [k for k in range(1, num_scenarios_total + 1)]
-
-SCENARIOS = np.sort(np.random.choice(np.arange(1, num_scenarios_total + 1), size=num_scenarios_total, replace=False))
-
-## Generate price scenarios using normal distribution
-price_scenarios_DA = {}
-price_scenarios_B = {}
-# plt.figure(figsize=(12, 12))
-k_x = 0
-for k in SCENARIOS_PRICE:
-    k_x = k_x + 1
-    plot_values_k = []
-    plot_values_B = []
+    ##
+    scenarios_data = {}
+    #first index (t): time
+    #second index (k): scenario
     for t in range(1, 25):
-        coeff_prices = abs(np.random.normal(1, 0.1))
-        price_scenarios_DA[t, k] = coeff_prices * prices_data_DA[t - 1]
-        price_scenarios_B[t, k] = coeff_prices * prices_data_B[t - 1]
-        plot_values_k.append(coeff_prices * prices_data_DA[t - 1])
-        plot_values_B.append(coeff_prices * prices_data_B[t - 1])
-    #plt.plot(range(0, 24), plot_values_k, linewidth=1.5, label=f'P{k_x}')
-    #plt.plot(range(0, 24), plot_values_B, linewidth=1.5, label=f'P{k_x}')
+        for k in range(1, num_scenarios_total + 1):
+            k_p = SCENARIOS_PRICE[((k - 1) // num_scenarios_wind) % num_price_scenarios]
+            k_w = SCENARIOS_WIND[(k - 1) % num_scenarios_wind]
+            scenarios_data[(t, k)] = [price_scenarios[(t, k_p)], wind_power_scenarios[(t, k_w)]]
 
-plt.figure()
-plt.plot(range(0, 24), plot_values_k, linewidth=1.5, label=f'P{k_x}')
-plt.xlabel('Time (hours)', fontsize=12)
-plt.ylabel('Electricity Price (EUR/MWh)', fontsize=12)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.grid(True)
-plt.title('Price scenarios DA', fontsize=12)
-plt.legend(fontsize=7, loc='upper left')
-plt.show()
+    return scenarios_data
 
-plt.figure()
-plt.plot(range(0, 24), plot_values_B, linewidth=1.5, label=f'P{k_x}')
-plt.xlabel('Time (hours)', fontsize=12)
-plt.ylabel('Electricity Price (EUR/MWh)', fontsize=12)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.grid(True)
-plt.title('Price scenarios B', fontsize=12)
-plt.legend(fontsize=7, loc='upper left')
-plt.show()
+def createScenariosPlusPlot():
+    np.random.seed(1)
 
+    prices_data = [51.49, 48.39, 48.92, 49.45, 42.72, 50.84, 82.15, 100.96, 116.60, 112.20, 108.54, 111.61, 114.02,
+        127.40, 134.02, 142.18, 147.42, 155.91, 154.10, 148.30, 138.59, 129.44, 122.89, 112.47
 
-##
-#plt.size = (6, 6)
-#plt.xlim(0,24)
-plt.xlabel('Time (hours)', fontsize=12)
-plt.ylabel('Electricity Price (EUR/MWh)', fontsize=12)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.grid(True)
-plt.title('Price scenarios', fontsize=12)
-plt.legend(fontsize = 7, loc='upper left')
-#plt.savefig('figures/price_scenarios.png', dpi=600, bbox_inches='tight')
-plt.show()
+    ]
 
-### WT Power definition
-WIND_FARMS = ['WT1', 'WT2', 'WT3', 'WT4', 'WT5', 'WT6']
+    # TODO increase in necessary
+    num_price_scenarios = 10
+    num_scenarios_wind = 10
 
-dfs = {}
-wind_power_scenarios = {}
-p_max_wind = 48/6
+    SCENARIOS_PRICE = [f'"P{k}"' for k in range(1, num_price_scenarios + 1)]
+    SCENARIOS_WIND = [f'"V{k}"' for k in range(1, num_scenarios_wind + 1)]
 
-for k in range(1, 7):
-    file_path = f'data/wind {k}.out'
+    num_scenarios_total = num_price_scenarios * num_scenarios_wind
 
-    with open(file_path, 'r') as file:
-        file_contents = file.readlines()
-        data = [line.strip().split(",") for line in file_contents]
-        df = pd.DataFrame(data)
-        df = df.iloc[:, 1:]
-        df.columns = df.iloc[0]
-        df = df.iloc[1:25, :num_scenarios_wind]
-        df = df.reset_index(drop=True)
-        df = df.apply(pd.to_numeric, errors='coerce')
-        dfs[f'WT{k}'] = df * p_max_wind
+    SCENARIOS_TOT = [k for k in range(1, num_scenarios_total + 1)]
 
-# plt.figure(figsize=(12, 12))
-k_x = 0
-for k in SCENARIOS_WIND:
-    k_x = k_x + 1
-    plot_values_k = []
+    SCENARIOS = np.sort(
+        np.random.choice(np.arange(1, num_scenarios_total + 1), size=num_scenarios_total, replace=False))
+
+    ## Generate price scenarios using normal distribution
+    price_scenarios = {}
+    # plt.figure(figsize=(12, 12))
+    k_x = 0
+    for k in SCENARIOS_PRICE:
+        k_x = k_x + 1
+        plot_values_k = []
+        for t in range(1, 25):
+            coeff_prices = abs(np.random.normal(1, 0.1))
+            price_scenarios[t, k] = coeff_prices * prices_data[t - 1]
+            plot_values_k.append(coeff_prices * prices_data[t - 1])
+        plt.plot(range(0, 24), plot_values_k, linewidth=1.5, label=f'P{k_x}')
+
+    # plt.size = (6, 6)
+    # plt.xlim(0,24)
+    plt.xlabel('Time (hours)', fontsize=12)
+    plt.ylabel('Electricity Price (EUR/MWh)', fontsize=12)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(True)
+    plt.title('Price scenarios', fontsize=12)
+    plt.legend(fontsize=7, loc='upper left')
+    # plt.savefig('figures/price_scenarios.png', dpi=600, bbox_inches='tight')
+    plt.show()
+
+    ### WT Power definition
+    WIND_FARMS = ['WT1', 'WT2', 'WT3', 'WT4', 'WT5', 'WT6']
+
+    dfs = {}
+    wind_power_scenarios = {}
+    p_max_wind = 48 / 6
+
+    for k in range(1, 7):
+        file_path = f'data/wind {k}.out'
+
+        with open(file_path, 'r') as file:
+            file_contents = file.readlines()
+            data = [line.strip().split(",") for line in file_contents]
+            df = pd.DataFrame(data)
+            df = df.iloc[:, 1:]
+            df.columns = df.iloc[0]
+            df = df.iloc[1:25, :num_scenarios_wind]
+            df = df.reset_index(drop=True)
+            df = df.apply(pd.to_numeric, errors='coerce')
+            dfs[f'WT{k}'] = df * p_max_wind
+
+    # plt.figure(figsize=(12, 12))
+    k_x = 0
+    for k in SCENARIOS_WIND:
+        k_x = k_x + 1
+        plot_values_k = []
+        for t in range(1, 25):
+            wind_power_scenarios[t, k] = sum(dfs[wt][k][t - 1] for wt in WIND_FARMS)
+            plot_values_k.append(sum(dfs[wt][k][t - 1] for wt in WIND_FARMS))
+
+        plt.plot(range(0, 24), plot_values_k, linewidth=1.5, label=f'W{k_x}')
+
+    plt.xlim(0, 24)
+    plt.xlabel('Time (hours)', fontsize=12)
+    plt.ylabel('Real windfarm production (MWh)', fontsize=12)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(True)
+    plt.title('Wind farm production scenarios', fontsize=12)
+    plt.legend(fontsize=7, loc='upper left')
+    # plt.savefig('figures/wind_scenarios.png', dpi=600, bbox_inches='tight')
+    plt.show()
+
+    ##
+    scenarios_data = {}
+    # first index (t): time
+    # second index (k): scenario
     for t in range(1, 25):
-        wind_power_scenarios[t, k] = sum(dfs[wt][k][t - 1] for wt in WIND_FARMS)
-        plot_values_k.append(sum(dfs[wt][k][t - 1] for wt in WIND_FARMS))
+        for k in range(1, num_scenarios_total + 1):
+            k_p = SCENARIOS_PRICE[((k - 1) // num_scenarios_wind) % num_price_scenarios]
+            k_w = SCENARIOS_WIND[(k - 1) % num_scenarios_wind]
+            scenarios_data[(t, k)] = [price_scenarios[(t, k_p)], wind_power_scenarios[(t, k_w)]]
 
-    plt.plot(range(0,24), plot_values_k, linewidth=1.5, label=f'W{k_x}')
 
-plt.xlim(0,24)
-plt.xlabel('Time (hours)', fontsize=12)
-plt.ylabel('Real windfarm production (MWh)', fontsize=12)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
-plt.grid(True)
-plt.title('Wind farm production scenarios', fontsize=12)
-plt.legend(fontsize=7, loc='upper left')
-#plt.savefig('figures/wind_scenarios.png', dpi=600, bbox_inches='tight')
-plt.show()
-
-##
-scenarios_data_DAprices = {}
-scenarios_data_Bprices = {}
-scenarios_data_WindProd = {}
-#first index (t): time      - fist value: price
-#second index (k): scenario - second value - wind production
-for t in range(1, 25):
-    for k in range(1, num_scenarios_total + 1):
-        k_p = SCENARIOS_PRICE[((k - 1) // num_scenarios_wind) % num_price_scenarios]
-        k_w = SCENARIOS_WIND[(k - 1) % num_scenarios_wind]
-        scenarios_data_DAprices[(t,k)] = [price_scenarios_DA[(t, k_p)]]
-        scenarios_data_Bprices[(t,k)] [price_scenarios_B[(t, k_p)]]
-        scenarios_data_WindProd[(t,k)] [wind_power_scenarios[(t, k_w)]]
-        #scenarios_data_OS[(t, k)] = [price_scenarios[(t, k_p)], wind_power_scenarios[(t, k_w)]]
-
+if __name__ == '__main__':
+    print(createScenarios())
--- two_stage_stochastic.py
+++ two_stage_stochastic.py
@@ -1,5 +1,6 @@
 import gurobipy as gp
 from gurobipy import GRB
+import scenario_generation as scenario_gen
 
 
 class Expando(object):
@@ -17,7 +18,8 @@ class InputData:
             TIME: list,
             generator_cost: float,
             generator_capacity: dict[int, dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float]],
-            price: dict[int, dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float]],
+            B_price: dict[int, dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float] | dict[int, float]],
+            DA_price: float,
             pi: dict[int, float],
             rho_charge: float,
             rho_discharge: float,
@@ -32,8 +34,10 @@ class InputData:
         self.generator_cost = generator_cost
         # Generators capacity (P^G_i)
         self.generator_capacity = generator_capacity
-        # Market clearing price (lambda)
-        self.price = price
+        # Market clearing price DA(lambda_DA)
+        self.DA_price = DA_price
+        # Market clearing price Balancing Market (lambda_B)
+        self.B_price = B_price
         # Scenario probability
         self.pi = pi
         #rho charge
@@ -55,94 +59,123 @@ class StochasticOfferingStrategy():
         self.results = Expando()
         self._build_model()
 
+    # Implementing only one Electricity production per Hour covers the case of not having perfect information
     def _build_variables(self):
         self.variables.generator_production = {
-            (t, k): self.model.addVar(
-                lb=0, ub=GRB.INFINITY, name='Electricity production'
+            (t): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Electricity production_h_{t}'
             )
             for t in self.data.TIME
-            for k in self.data.SCENARIOS
         }
 
         self.variables.charging_power = {
-            (t, k): self.model.addVar(
-                lb=0, ub=GRB.INFINITY, name='Charging power'
+            (t): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Charging power_h_{t}'
             )
             for t in self.data.TIME
-            for k in self.data.SCENARIOS
         }
 
         self.variables.discharging_power = {
-            (t, k): self.model.addVar(
-                lb=0, ub=GRB.INFINITY, name='Discharging power'
+            (t): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Discharging power_h_{t}'
             )
             for t in self.data.TIME
-            for k in self.data.SCENARIOS
         }
 
         self.variables.soc = {
             (t, k): self.model.addVar(
-                lb=0, ub=GRB.INFINITY, name='State of charge'
+                lb=0, ub=GRB.INFINITY, name=f'State of charge_{t}_{k}'
+            )
+            for k in self.data.SCENARIOS for t in self.data.TIME
+
+        }
+
+        self.variables.balancing_discharge = {
+            (t, k): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Balancing_discharge_{t}_{k}'
+            )
+            for k in self.data.SCENARIOS for t in self.data.TIME
+
+        }
+
+        self.variables.balancing_charge = {
+            (t, k): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Balancing_charge_{t}_{k}'
+            ) for k in self.data.SCENARIOS for t in self.data.TIME
+
+        }
+
+        self.variables.balancing_power = {
+            (t, k): self.model.addVar(
+                lb=0, ub=GRB.INFINITY, name=f'Balancing power_{t}_{k}'
+            )for k in self.data.SCENARIOS for t in self.data.TIME
+        }
+
+    def _build_constraints(self):
+
+
+        self.constraints.balancing_power = {
+            (t,k): self.model.addLConstr(
+                self.data.generator_capacity[t][k] - self.variables.generator_production[t] - self.variables.balancing_charge[(t,k)] + self.variables.balancing_discharge[(t,k)],
+                GRB.EQUAL,
+                self.variables.balancing_power[(t,k)]
             )
             for t in self.data.TIME
             for k in self.data.SCENARIOS
         }
-
-    def _build_constraints(self):
         self.constraints.total_power_constraint = {
             (t, k): self.model.addLConstr(
-                self.data.generator_capacity[t][k] - self.variables.charging_power[(t,k)] + self.variables.discharging_power[(t,k)],
+                self.data.generator_capacity[t][k] - self.variables.charging_power[(t)] + self.variables.discharging_power[(t)],
                 GRB.EQUAL,
-                self.variables.generator_production[(t,k)],
-                name=f'Max production constraint_{k}_{t}'
+                self.variables.generator_production[(t)],
+                name=f'Max production constraint_{t}_{k}'
             )
             for t in self.data.TIME
             for k in self.data.SCENARIOS
         }
 
         self.constraints.max_production_constraints = {
-            (k, t): self.model.addLConstr(
-                self.variables.charging_power[(t,k)],
+            (t, k): self.model.addLConstr(
+                self.variables.charging_power[(t)] + self.variables.balancing_charge[(t,k)],
                 GRB.LESS_EQUAL,
                 self.data.generator_capacity[t][k],
-                name=f'Max production constraint_{k}_{t}'
+                name=f'Max production constraint_{t}_{k}'
             )
             for k in self.data.SCENARIOS
             for t in self.data.TIME
         }
 
         self.constraints.SOC_max = {
-            (k, t): self.model.addLConstr(
+            (t, k): self.model.addLConstr(
                 self.variables.soc[(t,k)],
                 GRB.LESS_EQUAL,
                 self.data.soc_max,
-                name=f'Max state of charge constraint_{k}_{t}'
-            )
+                name=f'Max state of charge constraint_{t}_{k}'
+            ) for t in self.data.TIME
             for k in self.data.SCENARIOS
-            for t in self.data.TIME
         }
 
         self.constraints.SOC_time = {
-            (k, t): self.model.addLConstr(
-                self.variables.soc[k][t-1] + self.variables.charging_power[k][t] * self.data.rho_charge - self.variables.discharging_power[k][t] * (1/self.data.rho_discharge),
+            (t, k): self.model.addLConstr(
+                self.variables.soc[(k,t-1)] + (self.variables.charging_power[(t)] + self.variables.balancing_charge[(t,k)])* self.data.rho_charge - (self.variables.discharging_power[(t)]+self.variables.balancing_discharge[(t,k)]) * (1/self.data.rho_discharge),
                 GRB.EQUAL,
-                self.variables.soc[k][t],
-                name=f'SOC time constraint_{k}_{t}'
+                self.variables.soc[(t,k)],
+                name=f'SOC time constraint_{t}_{k}'
             )
-            for k in self.data.SCENARIOS
             for t in self.data.TIME
+            for k in self.data.SCENARIOS
             if t > 1
         }
 
         self.constraints.SOC_init = {
-            (k, t): self.model.addLConstr(
-                self.data.soc_init + self.variables.charging_power[k][t] * self.data.rho_charge - self.variables.discharging_power[k][t] * (1/self.data.rho_discharge),
+            (t, k): self.model.addLConstr(
+                self.data.soc_init + (self.variables.charging_power[(t)] + self.variables.balancing_charge[(t,k)]) * self.data.rho_charge - (self.variables.discharging_power[(t)]+self.variables.balancing_discharge[(t,k)]) * (1/self.data.rho_discharge),
                 GRB.EQUAL,
-                self.variables.soc[k][1],
-                name=f'SOC initial constraint{k}_{t}'
+                self.variables.soc[(k,1)],
+                name=f'SOC initial constraint{t}_{k}'
             )
-            for k in self.data.SCENARIOS
             for t in self.data.TIME
+            for k in self.data.SCENARIOS
         }
 
 
@@ -150,21 +183,20 @@ class StochasticOfferingStrategy():
         objective = (
             gp.quicksum(
                 gp.quicksum(
-                    self.variables.generator_production[k][t] * self.data.pi[k] * (
-                            self.data.price[k][t] - self.data.generator_cost)
-                    for t in self.data.TIME
+                    (self.data.DA_price-self.data.generator_cost)*self.variables.generator_production[t]+ self.data.pi[k]*(self.data.B_price[t][k]-self.data.generator_cost)*self.variables.balancing_power[(t,k)]
+                        for k in self.data.SCENARIOS
                 )
-                for k in self.data.SCENARIOS
+                for t in self.data.TIME
             )
         )
         self.model.setObjective(objective, GRB.MAXIMIZE)
 
     def _build_model(self):
-        self.model = gp.Model(name='One-stage stochastic offering strategy')
+        self.model = gp.Model(name='Two-stage stochastic offering strategy')
         self._build_variables()
         self._build_constraints()
         self._build_objective_function()
-        self.model.update()
+        #self.model.update()
 
     def _save_results(self):
         self.results.objective_value = self.model.ObjVal
@@ -201,37 +233,54 @@ class StochasticOfferingStrategy():
         print("--------------------------------------------------")
 
 if __name__ == '__main__':
-    generator_capacity_values = {
-        1: {1: 88.31, 2: 136.25, 3: 85.61, 4: 137.09, 5: 146.05},
-        2: {1: 134.67, 2: 96.76, 3: 139.7, 4: 96.58, 5: 117.66},
-        3: {1: 112.06, 2: 86.55, 3: 79.03, 4: 115.01, 5: 115.76},
-        4: {1: 85.12, 2: 122.11, 3: 83.61, 4: 80.92, 5: 90.75},
-        5: {1: 111.76, 2: 141.14, 3: 135.55, 4: 75.47, 5: 118.62}
-    }
 
-    price_values =  {
-        1: {1: 88.31, 2: 136.25, 3: 85.61, 4: 137.09, 5: 146.05},
-        2: {1: 134.67, 2: 96.76, 3: 139.7, 4: 96.58, 5: 117.66},
-        3: {1: 112.06, 2: 86.55, 3: 79.03, 4: 115.01, 5: 115.76},
-        4: {1: 85.12, 2: 122.11, 3: 83.61, 4: 80.92, 5: 90.75},
-        5: {1: 111.76, 2: 141.14, 3: 135.55, 4: 75.47, 5: 118.62}
-    }
 
-    input_data = InputData(
-        SCENARIOS=[1, 2, 3, 4, 5],
-        TIME=[1, 2, 3, 4, 5],
-        generator_cost=15,
-        generator_capacity=generator_capacity_values,
-        price=price_values,
-        pi={1: 0.2, 2: 0.2, 3: 0.25, 4: 0.25, 5: 0.25},
-        rho_charge=1.5,
-        rho_discharge=1.5,
-        soc_max = 500,
-        soc_init=50
-    )
-    model = StochasticOfferingStrategy(input_data)
-    model.run()
-    model.display_results()
-    model_PI = StochasticOfferingStrategy(input_data)
-    model_PI.run()
-    model_PI.display_results()
+
+    testdata = False
+    if(testdata):
+
+        generator_capacity_values = {
+            1: {1: 88.31, 2: 136.25, 3: 85.61, 4: 137.09, 5: 146.05},
+            2: {1: 134.67, 2: 96.76, 3: 139.7, 4: 96.58, 5: 117.66},
+            3: {1: 112.06, 2: 86.55, 3: 79.03, 4: 115.01, 5: 115.76},
+            4: {1: 85.12, 2: 122.11, 3: 83.61, 4: 80.92, 5: 90.75},
+            5: {1: 111.76, 2: 141.14, 3: 135.55, 4: 75.47, 5: 118.62}
+        }
+
+        price_values =  {
+            1: {1: 88.31, 2: 136.25, 3: 85.61, 4: 137.09, 5: 146.05},
+            2: {1: 134.67, 2: 96.76, 3: 139.7, 4: 96.58, 5: 117.66},
+            3: {1: 112.06, 2: 86.55, 3: 79.03, 4: 115.01, 5: 115.76},
+            4: {1: 85.12, 2: 122.11, 3: 83.61, 4: 80.92, 5: 90.75},
+            5: {1: 111.76, 2: 141.14, 3: 135.55, 4: 75.47, 5: 118.62}
+        }
+
+        input_data = InputData(
+            SCENARIOS=[1, 2, 3, 4, 5],
+            TIME=[1, 2, 3, 4, 5],
+            generator_cost=15,
+            generator_capacity=generator_capacity_values,
+            DA_price= 10,
+            B_price=price_values,
+            pi={1: 0.2, 2: 0.2, 3: 0.25, 4: 0.25, 5: 0.25},
+            rho_charge=1.5,
+            rho_discharge=1.5,
+            soc_max = 500,
+            soc_init=50
+        )
+    else:
+        scenario_data = scenario_gen.createScenarios()
+
+        max_i = max(key[0] for key in scenario_data.keys())
+        max_j = max(key[1] for key in scenario_data.keys())
+        print(max_i, max_j)
+        scenarios = [j for j in range(1, max_j+1)]
+        time = [i for i in range(1, max_i+1)]
+        print(time)
+
+    #model = StochasticOfferingStrategy(input_data)
+    #model.run()
+    #model.display_results()
+    #model_PI = StochasticOfferingStrategy(input_data)
+    #model_PI.run()
+    #model_PI.display_results()
